d83d031157a5a6422493c0c4777477e2ef58f47b
Louis Tsang <kawingt2@gmail.com>
2025-02-22 08:47:04 +0800

    fixed force_words_id

--- sentence.py
+++ sentence.py
@@ -3,10 +3,13 @@ from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
 tokenizer = AutoTokenizer.from_pretrained("t5-base")
 model = AutoModelForSeq2SeqLM.from_pretrained("t5-base")
 
-def sentence_generator(word):
-    encoder_input_str = "Generate an English Sentences " 
+def sentence_generator(word: str):
+    encoder_input_str = "Generate a sentence in English." 
     input_ids = tokenizer(encoder_input_str, return_tensors="pt").input_ids
     force_words_ids = tokenizer(word, add_special_tokens=False).input_ids
+    # Convert force_words_ids to a list of lists
+    print(force_words_ids)
+    force_words_ids = [force_words_ids]
 
     outputs = model.generate(
         input_ids,
